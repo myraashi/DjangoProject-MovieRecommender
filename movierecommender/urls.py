3039dabf16d77796e7e090e60bb7a9c15e8df40d
myraashi <myraashi2140@gmail.com>
2023-05-09 03:55:37 -0400

    Update the Movie Recommendation

--- movierecommender/urls.py
+++ movierecommender/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path(route='',view=views.movie_recommendation_view,name='recommendations'),
+    path(route='app_url/movierecommender',view=views.movie_recommendation_view,name='recommendations')
     # route is a string contains a URL pattern
 ]
--- movierecommender/views.py
+++ movierecommender/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 # HINT: Create a view to provide movie recommendations list for the HTML template
 
+def generate_movies_context():
+        context = {}
+        recommended_count = Movie.objects.filter(
+            recommended=True
+        ).count()
+        if recommended_count ==0:
+            movies = Movie.objects.filter(watched=False).order_by('-vote_count')[:30]
+        else:
+            movies = Movie.objects.filter(watched=False).filter(recommended=True).order_by('-vote_count')[:30]
+        context['movie_list'] = movies
+        return context
+def movie_recommendation_view(request):
+    if request.method== "GET":
+        context = generate_movies_context()
+        return render(request,'movierecommender/movie_list.html',context)
